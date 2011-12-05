ba0272e6072737f4cc930d00524c87b93927577c
Javad Moghisi <j.moghisi@gmail.com>
2011-12-05 15:23:58 +0000

    add FitNesse test fixture for call plans

--- test/fixtures/com/acmetelecom/BillingSystemFixture.java
+++ test/fixtures/com/acmetelecom/BillingSystemFixture.java
@@ -2,6 +2,17 @@ package com.acmetelecom;
 
 import fit.ColumnFixture;
 
+import java.util.Date;
+
 public class BillingSystemFixture extends ColumnFixture {
+
+    public Date time;
+    public String callFrom;
+    public String callTo;
+    public String duration;
+
+    public double cost() {
+        return 0.0d;
+    }
 
 }
