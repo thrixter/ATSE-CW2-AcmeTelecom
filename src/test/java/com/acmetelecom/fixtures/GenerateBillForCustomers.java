1e838377386193b3804b755752791c2c1ee59bba
Javad Moghisi <j.moghisi@gmail.com>
2011-12-06 22:07:57 +0000

    FitNesse tests for Peak, Off-peak and Overlapping calls

---
+++ src/test/java/com/acmetelecom/fixtures/GenerateBillForCustomers.java
@@ -0,0 +1,16 @@
+package com.acmetelecom.fixtures;
+
+import fit.Fixture;
+import fit.Parse;
+
+/**
+ * User: javad
+ * Date: 06/12/2011
+ */
+public class GenerateBillForCustomers extends Fixture {
+
+    @Override
+    public void doTable(Parse table) {
+        super.doTable(table);
+    }
+}
