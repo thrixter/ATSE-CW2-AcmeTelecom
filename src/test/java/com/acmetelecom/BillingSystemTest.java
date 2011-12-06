3d4b200b2dd1ce5574875393a1c349bdfc280387
Javad Moghisi <j.moghisi@gmail.com>
2011-12-06 14:02:44 +0000

    add a dummy unit test

---
+++ src/test/java/com/acmetelecom/BillingSystemTest.java
@@ -0,0 +1,15 @@
+package com.acmetelecom;
+
+import org.junit.Test;
+
+/**
+ * User: javad
+ * Date: 06/12/2011
+ */
+public class BillingSystemTest {
+
+    @Test
+    public void testBillingSystem() {
+        
+    }
+}
